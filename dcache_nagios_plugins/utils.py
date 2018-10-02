c20f6888663d65ccc135a0ef9a0a3784522659fd
Petter A. Urkedal <urkedal@nbi.dk>
2018-10-02 14:41:18 +0200

    Remove unused definitions from utils.

--- dcache_nagios_plugins/utils.py
+++ dcache_nagios_plugins/utils.py
@@ -1,78 +1,9 @@
 # coding: utf-8
-import sys
 import math
 import re
 
 
-class ConfigurationError(Exception):
-    pass
-
-
-def fst(x):
-    return x[0]
-
-
-def snd(x):
-    return x[1]
-
-
-def error_at(path, lineno, msg, *args, **kwargs):
-    if args:
-        msg = msg % args
-    if kwargs:
-        msg = msg % kwargs
-    sys.stderr('%s:%d: %s' % (path, lineno, msg))
-
-
-def iter_tabfile(f, path, ncols, space_in_last=False):
-    fd = open(path)
-    if space_in_last:
-        maxsplit = ncols - 1
-    else:
-        maxsplit = -1
-    lineno = 0
-    have_error = False
-    for ln in fd:
-        lineno += 1
-        if ln.strip().startswith('#'):
-            continue
-        cols = ln.split(None, maxsplit)
-        if len(cols) != ncols:
-            error_at(path, lineno, "Expecting %d columns", ncols)
-            have_error = True
-        else:
-            f(cols)
-    if have_error:
-        raise ConfigurationError("Errors found in %s." % path)
-
-
-def tabfile_as_list(path, ncols, **kwargs):
-    xs = []
-    iter_tabfile(lambda x: xs.append(x), ncols, **kwargs)
-    return xs
-
-
-def tabfile_as_dict(get_key, get_val, path, ncols, **kwargs):
-    m = {}
-
-    def f(cols):
-        key = get_key(cols)
-        if key in m:
-            raise ConfigurationError("Duplicate key %s." % key)
-        m[key] = get_val(cols)
-
-    iter_tabfile(f, path, ncols, **kwargs)
-    return m
-
-
-class FreeObject(object):
-    def __init__(self, **kwargs):
-        for k, v in kwargs.iteritems():
-            setattr(self, k, v)
-
-
-_measured_re = re.compile(r'([+-]?[0-9.]+(?:[eE]-?[0-9]+)?)'
-                          '[_[:space:]]*(\S+)$')
+_measured_re = re.compile(r'([+-]?[0-9.]+(?:[eE]-?[0-9]+)?)[_[:space:]]*(\S+)$')
 _unit_by_log1024_size = {
     0: 'B', 1: 'KiB', 2: 'MiB', 3: 'GiB', 4: 'TiB', 5: 'PiB', 6: 'EiB'
 }
@@ -188,10 +119,3 @@ def counted_noun(count, sing_word, pl_word=None):
     if count == 1:
         return '%d %s' % (count, sing_word)
     return '%d %s' % (count, pl_word or sing_word + 's')
-
-
-def plcase(count, s, pl):
-    d = dict(count=count)
-    if count == 1:
-        return s % d
-    return pl % d
